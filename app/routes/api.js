b61fbf78a871bd70f1581b63ca2801ca915ebdec
Edifie <dilantaskin2@hotmail.com>
2022-10-15 13:21:12 +0100

    api fix

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,52 +1,48 @@
 var Form = require("../models/form");
 
-var app = require('')
 
+module.exports = function (router) {
+  // http://localhost:8080/api/forms
 
-// module.exports = function (router) {
-//   // http://localhost:8080/api/forms
+  router.post("/forms", function (req, res) {
+    // res.send('Testing users route');
+    var form = new Form();
 
-//   router.post("/forms", function (req, res) {
-//     // res.send('Testing users route');
-//     var form = new Form();
+    form.area = req.body.area;
+    form.responsive = req.body.responsive;
+    form.name = req.body.name;
+    form.tema = req.body.tema;
 
+    // res.json({
+    //   area : req.body.area,
+    //   responsive: req.body.responsive,
+    //   name: req.body.name,
+    //   tema: req.body.tema
+    // });
 
-//     form.area = req.body.area;
-//     form.responsive = req.body.responsive;
-//     form.name = req.body.name;
-//     form.tema = req.body.tema;
+    if (
+      req.body.area == null ||
+      req.body.responsive == null ||
+      req.body.name == null ||
+      req.body.area == "" ||
+      req.body.tema == null
+    ) {
+      res.json({
+        success: false,
+        message: "Ensure completing all the fields!",
+      });
+    } else {
+      form.save(function (err) {
+        //check if there is an error
+        if (err) {
+          res.json({ success: false, message: "Complete all the fields!" });
+          console.log(err);
+        } else {
+          res.json({ success: true, message: "Form created!" });
+        }
+      });
+    }
+  });
 
-    
-//     // res.json({ 
-//     //   area : req.body.area,
-//     //   responsive: req.body.responsive,
-//     //   name: req.body.name,
-//     //   tema: req.body.tema
-//     // });
-
-//     if (
-//       req.body.area == null ||
-//       req.body.responsive == null ||
-//       req.body.name == null ||
-//       req.body.area == "" ||
-//       req.body.tema == null
-//     ) {
-//       res.json({
-//         success: false,
-//         message: "Ensure completing all the fields!",
-//       });
-//     } else {
-//       form.save(function (err) {
-//         //check if there is an error
-//         if (err) {
-//           res.json({ success: false, message: "Complete all the fields!" });
-//           console.log(err);
-//         } else {
-//           res.json({ success: true, message: "Form created!" });
-//         }
-//       });
-//     }
-//   });
-
-//   return router;
+  return router;
 };
